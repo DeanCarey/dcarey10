e4e76cc02ea8ca0d19e09e1d85a68ac146441c4d
DeanCarey <dcarey10@stuy.edu>
2021-05-11 15:44:57 -0400

    k28

---
+++ 28_propagate/3/events.js
@@ -0,0 +1,29 @@
+// demo 3
+// JS event propagation
+
+var tds = document.getElementsByTagName('td');
+var trs = document.getElementsByTagName('tr');
+var table = document.getElementsByTagName('table')[0];
+
+var clicky = function(e) {
+  alert( this.innerHTML );
+  //Q: What will happen when next line is uncommented?
+  //e.stopPropagation();
+};
+
+for (var x=0; x < tds.length; x++) {
+  tds[x].addEventListener('click', clicky);
+}
+
+for (x=0; x < trs.length; x++) {
+  trs[x].addEventListener('click', clicky);
+}
+
+//Predict, then test...
+//Q: What effect does the boolean arg have?
+//   (Leave exactly 1 version uncommented to test...)
+
+table.addEventListener('click', clicky, true);
+//table.addEventListener('click', clicky, false);
+
+// Q: When user clicks on a cell, in what order will the pop-ups appear?
